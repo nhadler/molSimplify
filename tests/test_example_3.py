46df3c208d96e47014c2c9b578e04e15062d4e21
Ralf Meyer <meyer.ralf@yahoo.com>
2023-08-26 15:06:11 -0400

    Skipping example_3 + FF for now

--- tests/test_example_3.py
+++ tests/test_example_3.py
@@ -1,6 +1,9 @@
+import pytest
 import helperFuncs as hp
 
 
+@pytest.mark.skip("Skipping example 3 + FF as it is unclear to me if the core "
+                  "should be frozen during the final optimization. RM")
 def test_example_3(tmpdir):
     testName = "example_3"
     threshMLBL = 0.1
