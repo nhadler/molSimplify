2912003e80f44b2ddc0c8d9d4885c45d74e0bb0f
Ralf Meyer <meyer.ralf@yahoo.com>
2023-12-17 18:45:15 -0500

    Add references for the MOF csv files

--- tests/informatics/test_MOF_descriptors.py
+++ tests/informatics/test_MOF_descriptors.py
@@ -1,14 +1,15 @@
 import pytest
 import json
 import numpy as np
+import pandas as pd
 from molSimplify.Informatics.MOF.MOF_descriptors import get_MOF_descriptors
 from molSimplify.utils.timer import DebugTimer
 
 
 @pytest.fixture
-def ref_names(depth=3):
+def RACs_names(depth=3):
 
-    def RACs_names(starts, properties, depth, scope="all"):
+    def generate_names(starts, properties, depth, scope="all"):
         names = []
         for start in starts:
             for prop in properties:
@@ -21,28 +22,33 @@ def ref_names(depth=3):
 
     properties = ["chi", "Z", "I", "T", "S", "Gval"]
 
-    names = RACs_names(["f", "mc", "D_mc"], properties, depth)
+    names = generate_names(["f", "mc", "D_mc"], properties, depth)
     # f-lig does not include the "scope"
-    names.extend(RACs_names(["f-lig"], properties, depth, scope=None))
+    names.extend(generate_names(["f-lig"], properties, depth, scope=None))
 
     # Same for the starts that include the additional property alpha
     properties.append("alpha")
     names.extend(
-        RACs_names(["lc", "D_lc", "func", "D_func"], properties, depth))
+        generate_names(["lc", "D_lc", "func", "D_func"], properties, depth))
     return names
 
 
 @pytest.mark.parametrize(
-    "cif_file",
-    ["odac-21383.cif",
-     "odac-21433.cif",
-     "odac-21478.cif",
-     "odac-21735.cif",
-     "odac-21816.cif"])
-def test_get_MOF_descriptors(resource_path_root, tmpdir, cif_file, ref_names):
+    "name",
+    [
+        "odac-21383",
+        "odac-21433",
+        "odac-21478",
+        "odac-21735",
+        "odac-21816",
+    ])
+def test_get_MOF_descriptors(resource_path_root, tmpdir, name, RACs_names):
+    # NOTE All the .cif files were converted to primitive unit cell using the
+    # MOF_descriptors.get_primitive() function
+
     with DebugTimer("get_MOF_descriptors()"):
         full_names, full_descriptors = get_MOF_descriptors(
-            str(resource_path_root / "inputs" / "cif_files" / cif_file),
+            str(resource_path_root / "inputs" / "cif_files" / f"{name}.cif"),
             depth=3,
             path=str(tmpdir),
             xyzpath=str(tmpdir / "test.xyz"),
@@ -50,8 +56,16 @@ def test_get_MOF_descriptors(resource_path_root, tmpdir, cif_file, ref_names):
         )
 
     with open(resource_path_root / "refs" / "MOF_descriptors"
-              / cif_file.replace("cif", "json"), "r") as fin:
+              / name / f"{name}.json", "r") as fin:
         ref = json.load(fin)
 
-    assert full_names == ref_names
+    assert full_names == RACs_names
     np.testing.assert_allclose(full_descriptors, ref["descriptors"], atol=1e-6)
+
+    lc_descriptors = pd.read_csv(tmpdir / "lc_descriptors.csv")
+    lc_ref = pd.read_csv(resource_path_root / "refs" / "MOF_descriptors" / name / "lc_descriptors.csv")
+    assert all(lc_descriptors == lc_ref)
+
+    sbu_descriptors = pd.read_csv(tmpdir / "sbu_descriptors.csv")
+    sbu_ref = pd.read_csv(resource_path_root / "refs" / "MOF_descriptors" / name / "sbu_descriptors.csv")
+    assert all(sbu_descriptors == sbu_ref)
