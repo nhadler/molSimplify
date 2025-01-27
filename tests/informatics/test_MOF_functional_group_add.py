d0e06df072c46bf771e9de6fe8b07b4ea7dde629
gianmarco-terrones <gianmarcogterrones@gmail.com>
2025-01-27 15:16:27 -0500

    added more missing imports

--- tests/informatics/test_MOF_functional_group_add.py
+++ tests/informatics/test_MOF_functional_group_add.py
@@ -1,6 +1,7 @@
 import pytest
 from molSimplify.Informatics.MOF.MOF_functionalizer import functionalize_MOF
 from molSimplify.Informatics.MOF.PBC_functions import readcif
+import numpy as np
 
 @pytest.mark.parametrize(
     "num_func, func_group",
@@ -26,7 +27,7 @@ def test_fg_addition(resource_path_root, tmpdir, num_func, func_group):
         destination_path,
         path_between_functionalizations=3,
         functionalization_limit=num_func,
-        functional_group=func
+        functional_group=func_group
         )
 
     # Check the structure with functional groups added
