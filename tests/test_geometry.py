062201112bf2293ca276a673df281b244caa53a7
gianmarco-terrones <gianmarcogterrones@gmail.com>
2025-04-03 18:40:51 -0400

    new unit test for reflect_through_plane

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -3,6 +3,7 @@ from molSimplify.Scripts.geometry import (norm,
                                           normalize,
                                           checkplanar,
                                           dihedral,
+                                          reflect_through_plane,
                                           )
 from molSimplify.Classes.mol3D import mol3D
 from molSimplify.Classes.atom3D import atom3D
@@ -42,3 +43,25 @@ def test_dihedral():
 
     d = dihedral(mol, 0, 1, 2, 3)
     assert abs(d - 45.0) < 1e-6
+
+
+def test_reflect_through_plane():
+    mol = mol3D()
+    symbols = ['O','H','H']
+    coords = [
+    [-3.73751, 1.29708, 0.00050],
+    [-2.74818, 1.33993, -0.00035],
+    [-4.02687, 2.24392, -0.01831],
+    ]
+    for sym, coord in zip(symbols, coords):
+        mol.addAtom(atom3D(Sym=sym, xyz=coord))
+
+    rmol = reflect_through_plane(mol, [0,0,1], [0,0,-1])
+    ref_coords = np.array([
+        [-3.73751, 1.29708, -2.0005],
+        [-2.74818, 1.33993, -1.99965],
+        [-4.02687, 2.24392, -1.98169],
+        ])
+
+    coords = rmol.get_coordinate_array()
+    assert np.allclose(coords, ref_coords, atol=1e-5)
